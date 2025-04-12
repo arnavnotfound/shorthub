d8f3ea667fb838ab30a41028b514d154b22f4ec3
arnavnotfound <arnavchouhanofficial@gmail.com>
2025-04-12 11:06:23 +0530

    Env varaibel config in frontend

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,18 @@ from flask import Flask,jsonify
 from flask_pymongo import PyMongo
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+import os
+BASE_URL = os.getenv("BASE_URL")
+MONGO_URI = os.getenv("MONGO_URI")
+JWT_SCRET_KEY = os.getenv("JWT_SECRET_KEY")
+print(BASE_URL)
 
 # Initialize Flask app
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 db = PyMongo(app).db
 jwt = JWTManager(app)
-app.config['JWT_SECRET_KEY'] = 'your-secret-key' 
+app.config['JWT_SECRET_KEY'] = JWT_SCRET_KEY
 
 
 from routes.shortcut_routes import *  
